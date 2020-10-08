d33a0dcb212bb4fdd03a1a7b281c32679daafd2a
Kathryn Holloway <kathrynelholloway@gmail.com>
2020-10-08 16:53:37 +0100

    Added loading drinks from the DB

--- src/data/db.py
+++ src/data/db.py
@@ -1,20 +1,135 @@
-import pymysql
-# from .env import mysql_pass
-#NOTE: SQL playground database https://www.w3schools.com/sql/trysql.asp?filename=trysql_asc
+# import pymysql
+# # from .env import mysql_pass
+# #NOTE: SQL playground database https://www.w3schools.com/sql/trysql.asp?filename=trysql_asc
 
-def main():
-    connection = pymysql.connect(host = "localhost", port = 33066, user = "root", password = "password", db = "OrderApp" )
-    cursor = connection.cursor()
-    # cursor.execute("INSERT INTO drinks (drink_name) VALUES ('Rum')")
-    # connection.commit()
-    cursor.execute("SELECT drink_id, drink_name FROM drinks")
+# def main():
+#     connection = pymysql.connect(host = "localhost", port = 33066, user = "root", password = "password", db = "OrderApp" )
+#     cursor = connection.cursor()
+#     # cursor.execute("INSERT INTO drinks (drink_name) VALUES ('Rum')")
+#     # connection.commit()
+#     cursor.execute("SELECT drink_id, drink_name FROM drinks")
 
-    rows = cursor.fetchall()
-    for row in rows:
-        print(row)
+#     rows = cursor.fetchall()
+#     for row in rows:
+#         print(row)
     
-    cursor.close()
-    connection.close()
+#     cursor.close()
+#     connection.close()
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
+
+
+
+import pymysql
+
+# from src.constants import (
+#     DRINKS_FILE_PATH,
+#     PEOPLE_FILE_PATH,
+#     FAVOURITES_FILE_PATH
+# )
+# from src.data_store.file_store import File_Store
+# from src.models.person import Person
+# from src.models.drink import Drink
+
+# PERSON_ID_INDEX = 0
+# PERSON_FIRST_NAME_INDEX = 1
+# PERSON_LAST_NAME_INDEX = 2
+# PERSON_DRINK_NAME_INDEX = 3
+
+# PERSON_TABLE = "person"
+
+# PERSON_ID_COLUMN = "person_id"
+# PERSON_FIRST_NAME_COLUMN = "first_name"
+# PERSON_LAST_NAME_COLUMN = "surname"
+# PERSON_AGE_COLUMN = "age"
+
+# DRINK_ID_INDEX = 0
+# DRINK_NAME_INDEX = 1
+
+
+class MySQLDB:
+    def __init__(self, host='localhost', db_name="OrderApp", port=33066, user="root", password="password"):
+        self.__host = host
+        self.__db_name = db_name
+        self.__port = port
+        self.__user = user
+        self.__password = password
+
+    def __make_connection(self):
+        return pymysql.connect(host=self.__host,
+                               user=self.__user,
+                               password=self.__password,
+                               db=self.__db_name,
+                               port=self.__port)
+
+    # def load_people(self):
+    #     data = []
+    #     connection = self.__make_connection()
+    #     try:
+    #         with connection.cursor() as cursor:
+    #             sql = f'SELECT * FROM {PERSON_TABLE}'
+    #             cursor.execute(sql)
+    #             while True:
+    #                 person_data = cursor.fetchone()
+    #                 if not person_data:
+    #                     break
+    #                 data.append(Person(
+    #                             person_data[PERSON_ID_INDEX],
+    #                             person_data[PERSON_FIRST_NAME_INDEX],
+    #                             person_data[PERSON_LAST_NAME_INDEX],
+    #                             person_data[PERSON_DRINK_NAME_INDEX],
+    #                             ))
+    #         connection.commit()
+    #     finally:
+    #         connection.close()
+    #     return data
+
+    # def insert_person(self, person):
+    #     connection = self.__make_connection()
+    #     try:
+    #         with connection.cursor() as cursor:
+    #             data = [str(person.id), person.first_name,
+    #                     person.last_name, person.age]
+    #             sql = f'INSERT INTO {PERSON_TABLE} ({PERSON_ID_COLUMN}, {PERSON_FIRST_NAME_COLUMN}, \
+    #                 {PERSON_LAST_NAME_COLUMN}, {PERSON_AGE_COLUMN}) VALUES (%s, %s, %s, %s)'
+    #             cursor.execute(sql, data)
+    #             connection.commit()
+    #     finally:
+    #         connection.close()
+
+
+    # def insert_drink(self, drink):
+    #     connection = self.__make_connection()
+    #     print('doing stuff')
+    #     try:
+    #         with connection.cursor() as cursor:
+    #             data = [str(drink.id), drink.name]
+    #             sql = 'INSERT INTO drink (id, name) VALUES (%s, %s)'
+    #             cursor.execute(sql, data)
+    #             connection.commit()
+    #     finally:
+    #         connection.close()
+
+    # Load drinks - match input/output to save to file drinks function
+    def load_drinks(self):
+        data = {}
+        connection = self.__make_connection()
+        try:
+            with connection.cursor() as cursor:
+                sql = 'SELECT * from drinks'
+                cursor.execute(sql)
+                while True:
+                    drink_data = cursor.fetchone()
+                    if not drink_data:
+                        break
+                    data[drink_data[0]] = drink_data[1]
+                connection.commit()
+        finally:
+            connection.close()
+        return data
+
+
+database = MySQLDB()
+
+print(database.load_drinks())
