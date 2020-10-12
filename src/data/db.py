42b25be6cb4bd2525e169aeb03fbe04ee04dc48b
Kathryn Holloway <kathrynelholloway@gmail.com>
2020-10-12 21:33:30 +0100

    tidied up script comments

--- src/data/db.py
+++ src/data/db.py
@@ -23,31 +23,6 @@
 
 import pymysql
 
-# from src.constants import (
-#     DRINKS_FILE_PATH,
-#     PEOPLE_FILE_PATH,
-#     FAVOURITES_FILE_PATH
-# )
-# from src.data_store.file_store import File_Store
-# from src.models.person import Person
-# from src.models.drink import Drink
-
-# PERSON_ID_INDEX = 0
-# PERSON_FIRST_NAME_INDEX = 1
-# PERSON_LAST_NAME_INDEX = 2
-# PERSON_DRINK_NAME_INDEX = 3
-
-# PERSON_TABLE = "person"
-
-# PERSON_ID_COLUMN = "person_id"
-# PERSON_FIRST_NAME_COLUMN = "first_name"
-# PERSON_LAST_NAME_COLUMN = "surname"
-# PERSON_AGE_COLUMN = "age"
-
-# DRINK_ID_INDEX = 0
-# DRINK_NAME_INDEX = 1
-
-
 class MySQLDB:
     def __init__(self, host='localhost', db_name="OrderApp", port=33066, user="root", password="password"):
         self.__host = host
@@ -62,42 +37,6 @@ class MySQLDB:
                                password=self.__password,
                                db=self.__db_name,
                                port=self.__port)
-
-    # def load_people(self):
-    #     data = []
-    #     connection = self.__make_connection()
-    #     try:
-    #         with connection.cursor() as cursor:
-    #             sql = f'SELECT * FROM {PERSON_TABLE}'
-    #             cursor.execute(sql)
-    #             while True:
-    #                 person_data = cursor.fetchone()
-    #                 if not person_data:
-    #                     break
-    #                 data.append(Person(
-    #                             person_data[PERSON_ID_INDEX],
-    #                             person_data[PERSON_FIRST_NAME_INDEX],
-    #                             person_data[PERSON_LAST_NAME_INDEX],
-    #                             person_data[PERSON_DRINK_NAME_INDEX],
-    #                             ))
-    #         connection.commit()
-    #     finally:
-    #         connection.close()
-    #     return data
-
-    # def insert_person(self, person):
-    #     connection = self.__make_connection()
-    #     try:
-    #         with connection.cursor() as cursor:
-    #             data = [str(person.id), person.first_name,
-    #                     person.last_name, person.age]
-    #             sql = f'INSERT INTO {PERSON_TABLE} ({PERSON_ID_COLUMN}, {PERSON_FIRST_NAME_COLUMN}, \
-    #                 {PERSON_LAST_NAME_COLUMN}, {PERSON_AGE_COLUMN}) VALUES (%s, %s, %s, %s)'
-    #             cursor.execute(sql, data)
-    #             connection.commit()
-    #     finally:
-    #         connection.close()
-
 
     def save_person(self, person_id, person_name):
         connection = self.__make_connection()
